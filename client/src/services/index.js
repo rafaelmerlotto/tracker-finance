3db9fe9e19d6095ca683530dfe0ecb00b57552e1
root <root@Rafael-Merlotto>
2024-02-14 21:26:28 +0100

    deploy

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -3,7 +3,7 @@ import {ExpensesService} from "./expenses"
 import { IncomesService } from "./incomes";
 import { SavingsService } from "./savings";
 
-const url = 'http://localhost:4000';
+const url = 'https://finance-tracker-app-3hf8.onrender.com/';
 export const authService = new AuthService(`${url}/auth`)
 export const expensesService = new ExpensesService(`${url}/expenses`)
 export const incomesService = new IncomesService(`${url}/incomes`)
