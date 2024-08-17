e0b898abcef2b507e173250d00a137c59949a8d0
root <root@Rafael-Merlotto>
2024-08-17 18:15:20 +0200

    deploy server side on railway

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -4,7 +4,8 @@ import { IncomesService } from "./incomes";
 import { SavingsService } from "./savings";
 
 // const url = 'https://finance-tracker-app-3hf8.onrender.com';
-const url = 'http://localhost:4000';
+// const url = 'http://localhost:4000';
+const url = 'https://tracker-finance-production.up.railway.app';
 // const url = 'http://16.171.177.162';
 export const authService = new AuthService(`${url}/auth`)
 export const expensesService = new ExpensesService(`${url}/expenses`)
